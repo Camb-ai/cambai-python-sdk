2682ea414df0131e26494b9e6528c5685d0ae53f
arnavmehta7 <arnavmehta.cs@gmail.com>
2026-01-09 03:18:00 +0530

    version update

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="cambai-sdk",
-    version="0.1.0",
+    version="1.5.0",
     packages=find_packages(where="."),
     package_dir={"": "."},
     install_requires=[
         "httpx>=0.21.2",
         "pydantic>=1.10.0",
         "typing_extensions>=4.0.0",
+        "websockets>=11.0.3",
+        "websocket-client>=1.6.0",
     ],
     package_data={"camb": ["py.typed"]},
     include_package_data=True,
