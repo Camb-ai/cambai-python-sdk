d0aa6667553e1023db7a3666910e530ed2c88351
arnavmehta7 <arnavmehta.cs@gmail.com>
2026-01-09 14:36:48 +0530

    new version, model names updates

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="cambai-sdk",
-    version="1.5.0",
+    name="camb-sdk",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version="1.5.2",
     packages=find_packages(where="."),
     package_dir={"": "."},
     install_requires=[
