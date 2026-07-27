7574271eb3bf76fbd4d7cee816c9812f799c8c5a
arnavmehta7 <arnavmehta.cs@gmail.com>
2026-07-27 18:31:22 +0400

    chore: update versions

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="camb-sdk",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version="1.5.16",
+    version="1.5.17",
     packages=find_packages(where="."),
     package_dir={"": "."},
     install_requires=[
